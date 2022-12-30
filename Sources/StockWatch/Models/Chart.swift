4e90b4de37509d175d9ffc3c8fc4a800a5ccdcfd
Alex H <deampuleadd@yandex.com>
2022-12-30 09:49:22 -0800

    [ADDS][MODEL] adds decoding to chart model

--- Sources/StockWatch/Models/Chart.swift
+++ Sources/StockWatch/Models/Chart.swift
@@ -7,7 +7,64 @@
 
 import Foundation
 
-public struct Chart: Decodable
+public struct ChartData: Decodable {
+    
+    public let meta: ChartMeta
+    public let indicators: [Indicator]
+    
+    enum CodingKeys: CodingKey {
+        case meta
+        case timestamp
+        case indicators
+    }
+    
+    enum IndicatorsKeys: CodingKey {
+        case quote
+    }
+    
+    enum QuoteKeys: CodingKey {
+        case high
+        case close
+        case low
+        case open
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        meta = try container.decode(ChartMeta.self, forKey: .meta)
+        
+        let timestamps = try container.decodeIfPresent([Date].self, forKey: .timestamp) ?? []
+        if let indicatorsContainer = try? container.nestedContainer(keyedBy: IndicatorsKeys.self, forKey: .indicators),
+           var quotes = try? indicatorsContainer.nestedUnkeyedContainer(forKey: .quote),
+           let quoteContainer = try? quotes.nestedContainer(keyedBy: QuoteKeys.self) {
+            
+            
+            let highs = try quoteContainer.decodeIfPresent([Double?].self, forKey: .high) ?? []
+            let lows = try quoteContainer.decodeIfPresent([Double?].self, forKey: .low) ?? []
+            let opens = try quoteContainer.decodeIfPresent([Double?].self, forKey: .open) ?? []
+            let closes = try quoteContainer.decodeIfPresent([Double?].self, forKey: .close) ?? []
+            
+            indicators = timestamps.enumerated().compactMap { (offset, timestamp) in
+                guard
+                    let open = opens[offset],
+                    let low = lows[offset],
+                    let close = closes[offset],
+                    let high = highs[offset]
+                else { return nil}
+                return .init(timestamp: timestamp, open: open, high: high, low: low, close: close)
+            }
+        } else {
+            self.indicators = []
+        }
+    }
+    
+    public init(meta: ChartMeta, indicators: [Indicator]) {
+        self.meta = meta
+        self.indicators = indicators
+    }
+}
+
+public struct ChartMeta: Decodable
 {
     public let currency: String
     public let symbol: String
