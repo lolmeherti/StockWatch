7f36ae2a40e279e695f5435ee783ff146a37cc72
Alex H <deampuleadd@yandex.com>
2022-12-30 06:19:01 -0800

    [ADDS] executable

---
+++ Sources/ExecutableAPI/ExecutableAPI.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex H. on 30.12.22.
+//
+
+import Foundation
+import StockWatch
+
+@main
+struct ExecutableAPI
+{
+    static func main() async {
+        print(StockWatch().text)
+    }
+}
